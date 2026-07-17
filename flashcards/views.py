1d9850493268d35d209886ad10fd215067041016
joymarinho96-ops <joy.marinho96@gmail.com>
2026-07-16 22:32:35 -0300

    fix: resolve erro de importacao em views.py do flashcards

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_disconnect, get_list_or_404, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.db.models import Count, Q
