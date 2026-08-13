5338ebd2a72589bade6b42bcc8300f962c129896
joymarinho96-ops <joy.marinho96@gmail.com>
2026-08-13 16:43:10 -0300

    ESTATUTO: eliminar por completo carpeta y templates de medlatin

--- medlatin/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MedlatinConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "medlatin"
-    verbose_name = "MedLatin"
